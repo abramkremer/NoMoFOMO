a5b3232cd30491225bf8d2337a8a324e53e6597c
Abram Kremer <abramkremer1@gmail.com>
2022-09-11 13:08:44 -0400

    Uploading SwiftUI App

---
+++ NoMoFOMO/NoMoFOMO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoMoFOMO
+//
+//  Created by Abram Kremer on 9/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("SignIn")
+            .resizable()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoMoFOMO/NoMoFOMO/NoMoFOMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoMoFOMOApp.swift
+//  NoMoFOMO
+//
+//  Created by Abram Kremer on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NoMoFOMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
